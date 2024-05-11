7f22cf03ffd51acd6f92ccd4a74cc784f0588524
Mohammad Salim <mohammadsalim017427@gmail.com>
2024-05-10 23:24:59 -0700

    search functionality add in all foods section

--- src/Pages/AllFoods.jsx
+++ src/Pages/AllFoods.jsx
@@ -1,18 +1,38 @@
 import { useLoaderData } from "react-router-dom";
 import SingleFood from "../components/SingleFood";
+import { useState } from "react";
 
 
 const AllFoods = () => {
     const foods = useLoaderData();
+    const [searchFood,setSearchFood] = useState("");
+
+    const filterFoods = foods.filter(food=>
+      food.name.toLowerCase().includes(searchFood.toLowerCase())
+    );
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-4 p-2">
+       <div className="">
+           <button className="btn flex justify-center ml-10">
+           <input className="text-center"
+                type="text"
+                placeholder="Search for food..."
+                value={searchFood}
+                onChange={(e) => setSearchFood(e.target.value)}
+            />
+           </button>
+         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-4 p-2">
           {
-            foods.map(food=><SingleFood key={food._id}
-                food={food}>
-                
-            </SingleFood>)
+           searchFood===""? foods.map(food=><SingleFood key={food._id}
+            food={food}>
+            
+        </SingleFood>):filterFoods.map(food=><SingleFood
+        key={food._id}
+        food={food}
+        ></SingleFood>)
           }
         </div>
+       </div>
+      
     );
 };
 
