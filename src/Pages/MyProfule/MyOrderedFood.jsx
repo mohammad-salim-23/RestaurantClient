b92897d79f305ef7b0180d317d6cc668ec7df8d4
Mohammad Salim <mohammadsalim017427@gmail.com>
2024-05-11 10:05:54 -0700

    MyOrder Food page delete button add

--- src/Pages/MyProfule/MyOrderedFood.jsx
+++ src/Pages/MyProfule/MyOrderedFood.jsx
@@ -1,10 +1,41 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../components/AuthContext/AuthProvider";
+import Swal from "sweetalert2";
 
 
 const MyOrderedFood = () => {
     const {user} = useContext(AuthContext);
     const [orders,setOrder] = useState([]);
+    const handleDelete= (_id)=>{
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!",
+          })
+          .then((result)=>{
+            if(result.isConfirmed){
+                fetch(`http://localhost:5000/purchase/${_id}`,{
+                    method:"DELETE"
+                })
+                .then((res)=>res.json())
+                .then((data)=>{
+                    if(data.deletedCount>0){
+                        Swal.fire({
+                            title: "Deleted!",
+                            text: "Your file has been deleted.",
+                            icon: "success",
+                          });  
+                   const remaining = orders.filter((order)=>order._id!==_id);
+                   setOrder(remaining);        
+                    }
+                })
+            }
+          })
+    }
     useEffect(()=>{
         if(user?.email){
             fetch(`http://localhost:5000/myPurchase/${user?.email}`)
@@ -22,13 +53,15 @@ const MyOrderedFood = () => {
                 orders.map((order,index)=>(
                     <div key={index}>
                     <div className="card card-side bg-base-100 shadow-xl h-96">
-  <figure><img src={order.image} alt="Movie"/></figure>
+  <figure><img className="w-96" src={order.image} alt="Movie"/></figure>
   <div className="card-body">
     <h2 className="font-bold text-xl ">{order.foodname}</h2>
     <p className="font-semibold">price: {order.price}$</p>
     <p className="font-medium">Buyer Name: {order.name}</p>
+    <h4>Buying Date: {order.currentDate}</h4>
     <div className="card-actions justify-end">
-      <button className="btn btn-primary">Watch</button>
+      <button onClick={()=>handleDelete(order._id)}
+       className="btn btn-outline bg-primaryColor">Delete</button>
     </div>
   </div>
 </div>
